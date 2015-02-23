c03e4af48ec54d3c141beb6145fe34b3d534e4a7
Jeff Casimir <jeff@casimircreative.com>
2015-02-23 15:19:54 -0700

    Add the ability to parse an entire file

--- lib/ripper.rb
+++ lib/ripper.rb
@@ -1,5 +1,40 @@
+require 'pry'
+
 class Ripper
   def parse_line(data)
-    data.downcase.scan(/\w+/)
+    data.downcase.scan(/[A-Za-z]+/)
+  end
+
+  def parse_file(target)
+
+    file = File.open(target)
+    doc = Document.new
+    file.each_line do |line|
+      content = line.strip
+      if content.length > 1
+        doc.add_words(parse_line(content))
+      end
+    end
+    doc
+  end
+end
+
+class Document
+  attr_reader :lines
+
+  def initialize
+    @lines = []
+  end
+
+  def line_count
+    lines.count
+  end
+
+  def line(number)
+    lines.at(number)
+  end
+
+  def add_words(set)
+    lines.push(set)
   end
 end
--- test/ripper_test.rb
+++ test/ripper_test.rb
@@ -25,4 +25,37 @@ class RipperTest < Minitest::Test
     assert words.include?("up")
     refute words.include?("up.")
   end
+
+  def test_parsing_removes_underscores
+    ripper = Ripper.new
+    words = ripper.parse_line("My spirits_, _as in a dream_.")
+    assert words.include?("spirits")
+    refute words.include?("spirits_")
+    assert words.include?("dream")
+    refute words.include?("dream_.")
+  end
+
+  def test_parsing_a_file
+    ripper = Ripper.new
+    document = ripper.parse_file('./test/fixtures/micro_tempest.txt')
+    assert_equal 5, document.line_count
+  end
+
+  def test_parsing_a_file_has_data_in_each_line
+    ripper = Ripper.new
+    document = ripper.parse_file('./test/fixtures/micro_tempest.txt')
+    words = document.line(4)
+    assert_equal 7, words.count
+    assert words.include?("boatswain")
+  end
+
+  # THE TEMPEST.
+  #
+  # ACT I.
+  #
+  # SCENE I. _On a ship at sea: a tempestuous noise of thunder
+  # and lightning heard._
+  #
+  # _Enter _a Ship-Master_ and _a Boatswain_._
+  #
 end
