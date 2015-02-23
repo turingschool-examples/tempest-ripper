1597a6f0cd068e2f79e301bab9615d1072fb127f
Jeff Casimir <jeff@casimircreative.com>
2015-02-23 15:52:09 -0700

    Removing hash storage, living the dream with a binary tree

--- lib/document.rb
+++ lib/document.rb
@@ -1,12 +1,11 @@
 require './lib/tree'
 
 class Document
-  attr_reader :lines, :words, :words_tree
+  attr_reader :lines, :words
 
   def initialize
     @lines = []
-    @words = {}
-    @words_tree = Tree.new
+    @words = Tree.new
   end
 
   def line_count
@@ -19,20 +18,14 @@ class Document
 
   def add_words(set)
     lines.push(set)
-
     line_number = lines.count - 1
 
     set.each do |word|
-      words[word] = [] unless words[word]
-      words[word].push(line_number)
-
-      words_tree.insert(word, line_number)
+      words.insert(word, line_number)
     end
   end
 
   def find_locations_for(word)
-    words_tree.find(word)
-    output = words[word]
-    return output
+    words.find(word)
   end
 end
